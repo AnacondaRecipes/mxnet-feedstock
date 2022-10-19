184d1b70f5cf41c525fc2bca2e28c5c858ce20ec
Serhii Kupriienko <skupriienko@anaconda.com>
2022-10-19 13:48:13 +0300

    Fix libmxnet.dylib on osx

--- recipe/build-libmxnet.sh
+++ recipe/build-libmxnet.sh
@@ -23,7 +23,6 @@ fi
 
 # Set target specific options
 declare -a anaconda_build_opts
-declare -a _build_opts
 case "${target_platform}" in
     linux-aarch64)
         anaconda_build_opts+=(-DUSE_OPENCV=ON)
@@ -81,11 +80,6 @@ else
   _gpu_opts+=(-DUSE_CUDNN=OFF)
 fi
 
-if [[ $(uname) != Darwin ]]; then
-      _build_opts+=(-DCMAKE_INSTALL_LIBDIR="lib")
-else
-      _build_opts+=()
-fi
 
 # Isolate the build.
   # rm -rf Build-${PKG_NAME}  # We could clean it up... But there really is no need.
@@ -100,7 +94,7 @@ cmake .. ${CMAKE_ARGS} \
     -LAH \
     -DCMAKE_BUILD_TYPE="Release" \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    "${_build_opts[@]}" \
+    -DCMAKE_INSTALL_LIBDIR="lib" \
     -DCMAKE_AR=${AR} \
     -DCMAKE_LINKER=${LD} \
     -DCMAKE_NM=${NM} \
@@ -132,6 +126,11 @@ echo "Building..."
 ninja -j${CPU_COUNT}
 ninja install
 
+if [[ $(uname) == Darwin ]]; then
+    ls -la
+    find ${PREFIX} | grep libmxnet.dylib | grep -v $PREFIX/lib/libmxnet.dylib | xargs rm -f
+    ln -sf $PREFIX/libmxnet.dylib $PREFIX/lib/libmxnet.dylib
+fi
 
 # install misses this file
 if [[ $target_platform != linux-s390x ]]; then
